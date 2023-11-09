1522c449289b789e5e5d72c48822297bb4e695df
AdrianSR30 <adrian.solis201@tectijuana.edu.mx>
2023-11-09 22:24:34 +0000

    agregado de forms.py

---
+++ collection/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class CollectionForm(forms.Form):
+    name = forms.CharField(label="Nombre", max_length=80, required=True)
+    description = forms.CharField(label="Descripción", required=True)
